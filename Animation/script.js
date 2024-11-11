af16267cac77f8acc8569ba50328153dbe91ab4a
showmik06 <sdhar6@lsu.edu>
2024-11-11 09:30:40 -0600

    some adaptation

--- Animation/script.js
+++ Animation/script.js
@@ -31,8 +31,8 @@ init()
 var crsr = document.querySelector(".cursor")
 var main = document.querySelector(".main")
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x + 20+"px"
-    crsr.style.top = dets.y + 20+"px"
+    crsr.style.left = dets.x + 5+"px"
+    crsr.style.top = dets.y + 5+"px"
 })
 
 
